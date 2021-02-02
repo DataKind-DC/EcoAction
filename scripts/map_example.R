841aec1733d55159f4f2e74f4b82601b51c6dd1a
icanhazcodeplz <magnusson.brent@gmail.com>
2021-02-01 21:18:14 -0800

    Also load planted_trees and civ_assoc

--- scripts/map_example.R
+++ scripts/map_example.R
@@ -7,7 +7,16 @@ library(leaflet)
 
 source("src/load_data.R")
 
+# Demographics and canopy data (only available at block_group and tract level)
 geography <- "block_group" # Must be "block_group" or "tract"
 data_df <- load_geo_data_for_map("tract")
 
-# Make fancy maps!
+# For civic assocations, you'll need to use this
+# civ_assoc <- read_geos_civ_assoc()
+
+# Trees planted by EcoAction -------------
+planted_trees <- read_tree_data_subset()
+
+# Make fancy maps! -----------------
+
+
