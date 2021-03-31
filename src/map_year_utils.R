c0270a3c15dd80c7744e1b7b9f6faefb5cecda09
J. Allen Baron <jabaron.phd@gmail.com>
2021-03-30 20:18:59 -0400

    Replace repeated addCircles() with for loop

--- src/map_year_utils.R
+++ src/map_year_utils.R
@@ -62,147 +62,28 @@ year_layer_map <-function(to_map, map_name){
 
     lf <- to_map %>%
         leaflet() %>%
-        addProviderTiles("CartoDB.Positron") %>%
-        addCircles( data =  to_map[ which(to_map$year==years[1]),"geometry"],
+        addProviderTiles("CartoDB.Positron")
+
+    for (i in seq_along(years)) {
+        lf <- lf %>%
+            addCircles(
+                data =  to_map[ which(to_map$year==years[i]),"geometry"],
                     color = "forestgreen",
                     weight=0.5,
                     radius = 25,
-                    opacity=0.5,
-                    fillOpacity=0.5,
+                    opacity = 0.5,
+                    fillOpacity = 0.5,
                     label = sprintf(
                         "<strong>%s</strong><br/>%s<br/>planted: %g",
-                        to_map[ which(to_map$year==years[1]),"tree_name"]$tree_name,
-                        to_map[ which(to_map$year==years[1]),"scientific_name"]$scientific_name,
-                        to_map[ which(to_map$year==years[1]),"year"]$year
+                        to_map[ which(to_map$year==years[i]),"tree_name"]$tree_name,
+                        to_map[ which(to_map$year==years[i]),"scientific_name"]$scientific_name,
+                        to_map[ which(to_map$year==years[i]),"year"]$year
                     ) %>% lapply(htmltools::HTML),
-                    group = label_groups[1]
-        )%>%
-        addCircles( data =  to_map[ which(to_map$year==years[2]),"geometry"],
-                    color = "forestgreen",
-                    weight=0.5,
-                    radius = 25,
-                    opacity=0.5,
-                    fillOpacity=0.5,
-                    label = sprintf(
-                        "<strong>%s</strong><br/>%s<br/>planted: %g",
-                        to_map[ which(to_map$year==years[2]),"tree_name"]$tree_name,
-                        to_map[ which(to_map$year==years[2]),"scientific_name"]$scientific_name,
-                        to_map[ which(to_map$year==years[2]),"year"]$year
-                    ) %>% lapply(htmltools::HTML),
-                    group = label_groups[2]
-        )%>%
-        addCircles( data =  to_map[ which(to_map$year==years[3]),"geometry"],
-                    color = "forestgreen",
-                    weight=0.5,
-                    radius = 25,
-                    opacity=0.5,
-                    fillOpacity=0.5,
-                    label = sprintf(
-                        "<strong>%s</strong><br/>%s<br/>planted: %g",
-                        to_map[ which(to_map$year==years[3]),"tree_name"]$tree_name,
-                        to_map[ which(to_map$year==years[3]),"scientific_name"]$scientific_name,
-                        to_map[ which(to_map$year==years[3]),"year"]$year
-                    ) %>% lapply(htmltools::HTML),
-                    group = label_groups[3]
-        )%>%
-        addCircles( data =  to_map[ which(to_map$year==years[4]),"geometry"],
-                    color = "forestgreen",
-                    weight=0.5,
-                    radius = 25,
-                    opacity=0.5,
-                    fillOpacity=0.5,
-                    label = sprintf(
-                        "<strong>%s</strong><br/>%s<br/>planted: %g",
-                        to_map[ which(to_map$year==years[4]),"tree_name"]$tree_name,
-                        to_map[ which(to_map$year==years[4]),"scientific_name"]$scientific_name,
-                        to_map[ which(to_map$year==years[4]),"year"]$year
-                    ) %>% lapply(htmltools::HTML),
-                    group = label_groups[4]
-        )%>%
-        addCircles( data =  to_map[ which(to_map$year==years[5]),"geometry"],
-                    color = "forestgreen",
-                    weight=0.5,
-                    radius = 25,
-                    opacity=0.5,
-                    fillOpacity=0.5,
-                    label = sprintf(
-                        "<strong>%s</strong><br/>%s<br/>planted: %g",
-                        to_map[ which(to_map$year==years[5]),"tree_name"]$tree_name,
-                        to_map[ which(to_map$year==years[5]),"scientific_name"]$scientific_name,
-                        to_map[ which(to_map$year==years[5]),"year"]$year
-                    ) %>% lapply(htmltools::HTML),
-                    group = label_groups[5]
-        )%>%
-        addCircles( data =  to_map[ which(to_map$year==years[6]),"geometry"],
-                    color = "forestgreen",
-                    weight=0.5,
-                    radius = 25,
-                    opacity=0.5,
-                    fillOpacity=0.5,
-                    label = sprintf(
-                        "<strong>%s</strong><br/>%s<br/>planted: %g",
-                        to_map[ which(to_map$year==years[6]),"tree_name"]$tree_name,
-                        to_map[ which(to_map$year==years[6]),"scientific_name"]$scientific_name,
-                        to_map[ which(to_map$year==years[6]),"year"]$year
-                    ) %>% lapply(htmltools::HTML),
-                    group = label_groups[6]
-        )%>%
-        addCircles( data =  to_map[ which(to_map$year==years[7]),"geometry"],
-                    color = "forestgreen",
-                    weight=0.5,
-                    radius = 25,
-                    opacity=0.5,
-                    fillOpacity=0.5,
-                    label = sprintf(
-                        "<strong>%s</strong><br/>%s<br/>planted: %g",
-                        to_map[ which(to_map$year==years[7]),"tree_name"]$tree_name,
-                        to_map[ which(to_map$year==years[7]),"scientific_name"]$scientific_name,
-                        to_map[ which(to_map$year==years[7]),"year"]$year
-                    ) %>% lapply(htmltools::HTML),
-                    group = label_groups[7]
-        )%>%
-        addCircles( data =  to_map[ which(to_map$year==years[8]),"geometry"],
-                    color = "forestgreen",
-                    weight=0.5,
-                    radius = 25,
-                    opacity=0.5,
-                    fillOpacity=0.5,
-                    label = sprintf(
-                        "<strong>%s</strong><br/>%s<br/>planted: %g",
-                        to_map[ which(to_map$year==years[8]),"tree_name"]$tree_name,
-                        to_map[ which(to_map$year==years[8]),"scientific_name"]$scientific_name,
-                        to_map[ which(to_map$year==years[8]),"year"]$year
-                    ) %>% lapply(htmltools::HTML),
-                    group = label_groups[8]
-        )%>%
-        addCircles( data =  to_map[ which(to_map$year==years[9]),"geometry"],
-                    color = "forestgreen",
-                    weight=0.5,
-                    radius = 25,
-                    opacity=0.5,
-                    fillOpacity=0.5,
-                    label = sprintf(
-                        "<strong>%s</strong><br/>%s<br/>planted: %g",
-                        to_map[ which(to_map$year==years[9]),"tree_name"]$tree_name,
-                        to_map[ which(to_map$year==years[9]),"scientific_name"]$scientific_name,
-                        to_map[ which(to_map$year==years[9]),"year"]$year
-                    ) %>% lapply(htmltools::HTML),
-                    group = label_groups[9]
-        )%>%
-        addCircles( data =  to_map[ which(to_map$year==years[10]),"geometry"],
-                    color = "forestgreen",
-                    weight=0.5,
-                    radius = 25,
-                    opacity=0.5,
-                    fillOpacity=0.5,
-                    label = sprintf(
-                        "<strong>%s</strong><br/>%s<br/>planted: %g",
-                        to_map[ which(to_map$year==years[10]),"tree_name"]$tree_name,
-                        to_map[ which(to_map$year==years[10]),"scientific_name"]$scientific_name,
-                        to_map[ which(to_map$year==years[10]),"year"]$year
-                    ) %>% lapply(htmltools::HTML),
-                    group = label_groups[10]
-        )%>%
+                    group = label_groups[i]
+        )
+    }
+
+    lf <- lf %>%
         # Layers control
         addLayersControl(
             overlayGroups = label_groups,
