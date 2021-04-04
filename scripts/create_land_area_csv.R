eea02e18fc55648f3e0e090f6773f51593ea6806
icanhazcodeplz <magnusson.brent@gmail.com>
2021-04-03 18:12:26 -0700

    Move function "area_of_top_on_base" to sf_helpers.R

--- scripts/create_land_area_csv.R
+++ scripts/create_land_area_csv.R
@@ -2,22 +2,9 @@ library(dplyr)
 library(sf)
 
 source("src/load_data.R")
+source("src/sf_helpers.R")
 
 # Calculate Tree Canopy Percentage ---------------------------------------------
-
-area_of_top_on_base <- function(base_df, top_df) {
-  # Units in m^2
-  # TODO: docstring
-  top_mpoly <- sf::st_union(top_df)
-  int <- tibble::as_tibble(sf::st_intersection(base_df, top_mpoly))
-  int$area_int <- sf::st_area(int$geometry) # Units of m^2
-
-  # Some clunky code to handle when there is no intersection in a poly of base_df
-  df <- dplyr::left_join(tibble::as_tibble(base_df), int, by = "geo_id")
-  df <- df$area_int
-  df[is.na(df)] <- 0
-  df
-}
 
 
 land_areas_and_pcts <- function(base_df) {
--- src/sf_helpers.R
+++ src/sf_helpers.R
@@ -24,3 +24,18 @@ get_poly_with_area <- function(sf_df) {
         rm_not_2dim %>%
         add_poly_area
 }
+
+
+area_of_top_on_base <- function(base_df, top_df) {
+    # Units in m^2
+    # TODO: docstring
+    top_mpoly <- sf::st_union(top_df)
+    int <- tibble::as_tibble(sf::st_intersection(base_df, top_mpoly))
+    int$area_int <- sf::st_area(int$geometry) # Units of m^2
+
+    # Some clunky code to handle when there is no intersection in a poly of base_df
+    df <- dplyr::left_join(tibble::as_tibble(base_df), int, by = "geo_id")
+    df <- df$area_int
+    df[is.na(df)] <- 0
+    df
+}
