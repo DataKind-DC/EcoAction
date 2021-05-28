0469ccedb9010ce06ec062434dd8d7a5476cc968
J. Allen Baron <jabaron.phd@gmail.com>
2021-05-27 21:29:25 -0400

    Add interpolate_bg_to_ca()
    Uses areal interpolation from `areal` package; will replace
    custom method used previously to calculate demographics for
    civic associations from ACS5 block group data
    
    This approach has the following advantages:
    - Ensures CRS of inputs is projected, not geographic, for
    more accurate math
    - Eliminates reliance on sf::st_intersection() which does not
    perform as expected

--- src/sf_helpers.R
+++ src/sf_helpers.R
@@ -1,6 +1,7 @@
 library(sf)
 library(dplyr)
 library(magrittr)
+library(areal)
 
 rm_not_2dim <- function(sf_df) {
     #' Removes non-polygons (i.e. not 2-dimensional
@@ -30,6 +31,72 @@ fix_sf_agr_error <- function(sf) {
     #' Fixes sf renaming problems
     sf %>%
         dplyr::mutate()
+}
+
+
+interpolate_bg_to_ca <- function(bg_sf, bg_id, ca_sf, ca_id, weight = "sum",
+                                 extensive, intensive, output = "tibble") {
+    #' Performs areal weighted interpolation (ensuring correct CRS)
+    #'
+    #' @param bg_sf A block group sf object with data to be interpolated
+    #' @param bg_id Unique block group identifier (most likely 'geo_id')
+    #' @param ca_sf A civic association sf object that data should be interpolated to
+    #' @param ca_id Unique civic association identifier
+    #' @params see areal::aw_interpolate() for on remaining parameter details
+
+    # identify all variables of interest (extensive or intensive)
+    if (missing(extensive)) {
+        var_interpolate <- intensive
+    } else if (missing(intensive)) {
+        var_interpolate <- extensive
+    } else {
+        var_interpolate <- union(extensive, intensive)
+    }
+
+    #### Change CRS for math ####
+    # capture input CRS (to revert)
+    ca_crs <- sf::st_crs(ca_sf)
+
+    # force projected CRS (same version used by Google Maps/OSM)
+    bg_merc <- sf::st_transform(bg_sf, "EPSG:3857")
+    ca_merc <- sf::st_transform(ca_sf, "EPSG:3857")
+
+    ##############################
+
+    # Validate inputs (return test results on failure)
+    if (!isTRUE(areal::ar_validate(bg_merc, ca_merc, var_interpolate))) {
+        stop(
+            "Input does not pass validation\n",
+            # print dataframe showing checks in error
+            areal::ar_validate(
+                bg_merc, ca_merc,
+                var_interpolate,
+                verbose = TRUE
+            ) %>%
+                as.data.frame() %>%
+                print() %>%
+                capture.output() %>%
+                paste0(collapse = "\n"),
+            call. = FALSE
+        )
+    }
+
+    ca_interp <- areal::aw_interpolate(
+        .data = ca_merc,
+        tid = {{ ca_id }},
+        source = bg_merc,
+        sid = {{ bg_id }},
+        weight = weight,
+        output = output,
+        extensive = extensive,
+        intensive = intensive
+    )
+
+    if(!missing(output) && output == "sf") {
+        sf::st_transform(ca_interp, crs = ca_crs)
+    } else {
+        ca_interp
+    }
 }
 
 
