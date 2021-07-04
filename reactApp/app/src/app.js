27f80096dcf8ce125668f2cc6a98418f1cbead44
icanhazcodeplz <magnusson.brent@gmail.com>
2021-07-04 14:42:39 -0700

    open plantable and trees working

--- reactApp/app/src/app.js
+++ reactApp/app/src/app.js
@@ -46,25 +46,29 @@ export default function App() {
   const [currentBlockGroup, setCurrentBlockGroup] = useState(null)
   const [blockGroupGeos, setBlockGroupGeos] = useState(null);
   const [blockGroupNamePlacements, setBlockGroupNamePlacements] = useState(null);
+  const [openPlantable, setOpenPlantable] = useState(null);
+  const [plantedTrees, setPlantedTrees] = useState(null);
   // const [blockGroupMeta, setBlockGroupMeta] = useState(null);
   //
-  // function setInitialViewport() {
-  //   const vp = new WebMercatorViewport(viewport);
-  //   const {longitude, latitude, zoom} = vp.fitBounds(
-  //     [
-  //       [minLng, minLat],
-  //       [maxLng, maxLat]
-  //     ],
-  //     {padding: 80}
-  //   );
-  //   console.log(longitude)
-  //   setViewport({
-  //     ...viewport,
-  //     longitude,
-  //     latitude,
-  //     zoom
-  //   })
-  // }
+  function arlingtonBoundsViewport() {
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    const vp = new WebMercatorViewport({
+      width: width,
+      height: height,
+    });
+    const {longitude, latitude, zoom} = vp.fitBounds(
+      [[minLng, minLat], [maxLng, maxLat]],
+      {padding: 30}
+    );
+    setViewport({
+      width: width,
+      height: height,
+      longitude: longitude,
+      latitude: latitude,
+      zoom: zoom,
+    })
+  }
 
   useEffect(() => {
     // TODO: can I do this outside an effect since I only need to do it once?
@@ -80,13 +84,27 @@ export default function App() {
         (data) => {
           setBlockGroupNamePlacements(data.data)
         })
-    fetch(`/api/blockgroupmeta`)
-      .then((res) => res.json())
-      .then(
-        (data) => {
-          setBlockGroupMeta(data.data)
-        })
+    // fetch(`/api/blockgroupmeta`)
+    //   .then((res) => res.json())
+    //   .then(
+    //     (data) => {
+    //       setBlockGroupMeta(data.data)
+    //     })
   }, [])
+
+  useEffect(() => {
+    if (currentBlockGroup) {
+
+      fetch(`/api/blockgroup/${currentBlockGroup.geo_id}`)
+        .then((res) => res.json())
+        .then(
+          (data) => {
+            setOpenPlantable(data.openPlantable.geometry)
+            setPlantedTrees(data.trees)
+          }
+        )
+    }
+  }, [currentBlockGroup])
 
   const layerStyle = {
     id: 'block-groups-line',
@@ -100,7 +118,6 @@ export default function App() {
     id: 'block-groups-fill',
     type: 'fill',
     paint: {
-      'fill-outline-color': '#0040c8',
       'fill-color': '#000000',
       'fill-opacity': 0.05
     }
@@ -116,29 +133,61 @@ export default function App() {
       'text-justify': 'auto',
     }
   }
-
+  const openPlantableStyle = {
+    id: 'open-plantable',
+    type: 'fill',
+    paint: {
+      'fill-outline-color': '#0040c8',
+      'fill-color': '#0080ff', // blue color fill
+      'fill-opacity': 0.3
+    }
+  }
+  const plantedTreesStyle = {
+    id: 'planted-trees',
+    type: 'circle',
+    paint: {
+      'circle-radius': {
+        'base': 1,
+        'stops': [
+          [12, 1.5],
+          [22, 18]
+        ]
+      },
+      'circle-color': [
+        'match',
+        ['get', 'treeCount'],
+        1, '#2e9209',
+        2, '#8aa313',
+        3, '#bb791d', 4, '#bb791d', 5, '#bb791d',
+        6, '#fb683b', 7, '#fb683b', 8, '#fb683b', 9, '#fb683b', 10, '#fb683b',
+        /* other */ '#c92828'
+      ]
+    }
+  }
   const onClick = event => {
     const feature = event.features[0];
     if (feature) {
+      const bgName = feature.properties.bg_name
+      const bgGeo = blockGroupGeos.features.find((bg) => bg.properties.bg_name === bgName)
       // calculate the bounding box of the feature
-      const [minLng, minLat, maxLng, maxLat] = bbox(feature);
+      const [minLng, minLat, maxLng, maxLat] = bbox(bgGeo);
       // construct a viewport instance from the current state
+      console.log(viewport)
       const vp = new WebMercatorViewport(viewport);
       const {longitude, latitude, zoom} = vp.fitBounds(
         [[minLng, minLat], [maxLng, maxLat]],
-        {padding: 80});
+        {padding: 20});
       setViewport({
         ...viewport,
         longitude,
         latitude,
         zoom,
         transitionInterpolator: new LinearInterpolator({
-          around: [event.offsetCenter.x, event.offsetCenter.y]
+          // around: [event.offsetCenter.x, event.offsetCenter.y]
         }),
         transitionDuration: 500
       })
       setCurrentBlockGroup(feature.properties)
-      console.log(feature)
     }
   };
 
@@ -149,11 +198,13 @@ export default function App() {
         {...viewport}
         width="100%"
         height="100%"
-        mapStyle="mapbox://styles/mapbox/light-v9"
+        mapStyle="mapbox://styles/brentmaggy/ckp9w138f35yi17t83b0xlklo"
+        // mapStyle="mapbox://styles/mapbox/light-v9"
         interactiveLayerIds={['block-groups-fill']}
         onViewportChange={setViewport}
         mapboxApiAccessToken={MAPBOX_TOKEN}
         onClick={onClick}
+        onLoad={arlingtonBoundsViewport}
       >
         <Source id="block-groups" type="geojson" data={blockGroupGeos}>
           <Layer {...layerStyle} />
@@ -161,6 +212,12 @@ export default function App() {
         </Source>
         <Source id="block-group-names" type="geojson" data={blockGroupNamePlacements}>
           <Layer {...blockGroupNamesStyle} />
+        </Source>
+        <Source id="open-plantable" type="geojson" data={openPlantable}>
+          <Layer {...openPlantableStyle} />
+        </Source>
+        <Source id="planted-trees" type="geojson" data={plantedTrees}>
+          <Layer {...plantedTreesStyle} />
         </Source>
         <Pins data={CITIES} onClick={setPopupInfo}/>
 
